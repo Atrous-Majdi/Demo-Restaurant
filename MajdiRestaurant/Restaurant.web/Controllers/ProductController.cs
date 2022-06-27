770361bbb50ea07a0be65d0b01dbd5c1e52bc8b2
Majdi Atrous <majdiatrous@gmail.com>
2022-06-27 12:49:37 +0200

    ProductController  + ProuctView to consume API and show data

---
+++ MajdiRestaurant/Restaurant.web/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.web.Models;
+using Restaurant.web.Services.IServices;
+
+namespace Restaurant.web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+          
+            var response = await _productService.GetProductsAsync<ResponseDto>();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
