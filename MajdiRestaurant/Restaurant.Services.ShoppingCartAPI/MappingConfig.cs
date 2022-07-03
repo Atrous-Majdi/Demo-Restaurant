0d7d45ee1fc3545788114dd6fa4b356db47ebdd2
Majdi Atrous <majdiatrous@gmail.com>
2022-07-04 01:45:59 +0200

    Configuring ShoppingCart API Program.cs

---
+++ MajdiRestaurant/Restaurant.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Restaurant.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return null;
+        }
+    }
+}
