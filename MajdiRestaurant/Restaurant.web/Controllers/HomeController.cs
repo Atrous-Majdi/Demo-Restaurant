c2b8356f4ba48dddc39673f6b5c361c81f223a9c
Majdi Atrous <majdiatrous@gmail.com>
2022-07-03 23:13:32 +0200

    Update HomeController Index to get Products using the Products API

--- MajdiRestaurant/Restaurant.web/Controllers/HomeController.cs
+++ MajdiRestaurant/Restaurant.web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Restaurant.web.Models;
+using Restaurant.web.Services.IServices;
 using System.Diagnostics;
 
 namespace Restaurant.web.Controllers
@@ -9,14 +11,23 @@ namespace Restaurant.web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            List<ProductDto> productsList = new List<ProductDto>();
+            var response = await _productService.GetProductsAsync<ResponseDto>("");
+            if (response != null && response.IsSuccess)
+            {
+                productsList = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result)); 
+            }
+
             return View();
         }
 
